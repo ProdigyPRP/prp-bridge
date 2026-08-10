2e8390cfb3c43a14f24840d1ce6f7ac4d00bac46
PsychoShock <94499608+PsychoShock@users.noreply.github.com>
2026-08-10 11:04:06 -0400

    tweak(config/bridge): missing alias

--- types/config.lua
+++ types/config.lua
@@ -10,7 +10,7 @@
 
 ---@alias AvailableMedicals 'qbx_medical' | 'esx_ambulancejob' | 'wasabi_ambulance' | 'ars_ambulancejob' | 'osp_ambulance' | 'p-ambulancejob' | 'nd_ambulance' | 'qb-ambulancejob' | 'randol_medical' | 'tk_ambulancejob' | false
 
----@alias AvailableDispatches 'ps-dispatch' | 'origen_police' | 'cd_dispatch' | 'rcore_dispatch' | 'tk_dispatch' | 'lb-tablet' | 'aty_dispatch' | 'codem-dispatch' | 'core_dispatch' | 'qs-dispatch' | false
+---@alias AvailableDispatches 'ps-dispatch' | 'origen_police' | 'cd_dispatch' | 'rcore_dispatch' | 'tk_dispatch' | 'lb-tablet' | 'aty_dispatch' | 'codem-dispatch' | 'core_dispatch' | 'kartik-mdt' | 'qs-dispatch' | false
 
 ---@alias AvailableMinigames 'prp-minigames' | false
 
